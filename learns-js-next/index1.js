9689d7b4ce0d8252d1a8d6d73341b2b193d8dd5f
Jakub Kowalczyk <s31559@pjwstk.edu.pl>
2025-08-26 21:22:21 +0200

    init

--- learns-js-next/index1.js
+++ learns-js-next/index1.js
@@ -8,3 +8,119 @@ let employee = {
 };
 
 console.log(employee.gateWage());
+
+const circle1 = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    draw: function(){
+        console.log('draw');
+    }
+};
+
+function createCircle(radius){
+    return {
+        radius,
+        draw: function(){
+            console.log('draw');
+        }
+    }
+}
+circle1.draw();
+
+const circle2 = createCircle(1);
+
+for( let key in circle1){
+    console.log(key, circle1[key]);
+}
+
+const keys = Object.keys(circle1);
+console.log(keys);
+
+if('radius' in circle1)
+    console.log('Circle has a radius.');
+
+
+function Circle3(radius){
+    this.radius = radius;
+
+    let defaultLocation = { x: 0 , y: 0 };
+
+    this.getdefaultLocation = function(){
+        return defaultLocation;
+    }
+
+    let computeOptimumLocation = function(){
+        // ...
+    }
+
+    this.draw = function(){
+        computeOptimumLocation();
+        console.log('draw3');
+    }
+
+    Object.defineProperties(this, {
+    defaultLocation: {
+        get: function() {
+            return defaultLocation;
+        },
+        set: function(value){
+            if(!value.x || !value.y)
+                throw new Error('Invalid location.')
+            defaultLocation = value;
+        }
+    }
+});
+}
+
+const another = new Circle3(3);
+
+another.draw();
+
+// ex 
+
+function Stopwatch(){
+    let duration , startTime , endTime, running= 0 ;
+    let isStarted = false;
+
+    this.start = function(){
+        if(!isStarted){
+            startTime = new Date();
+            isStarted = true;
+        }else{
+            throw new Error('Stopwatch is started');
+        }
+
+    }
+
+    this.stop = function(){
+        if(isStarted){
+            endTime = new Date();
+            isStarted = false;
+
+            const seconds = (endTime.getTime()- startTime.getTime()) / 1000;
+            duration += seconds;
+        }else{
+            throw new Error('Stopwatch is not started');
+        }
+    }
+
+    this.reset = function(){
+        startTime = null;
+        endTime = null;
+        isStarted = false;
+        duration = 0;
+    }
+
+    Object.defineProperties(this,{duration :{
+        get: function() { return duration; }
+    }
+    })
+}
+
+
+
+const sw = new Stopwatch();
+
